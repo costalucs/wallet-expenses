9160b963db1c6ae426d4541f3a9e8195fe2da02b
costalucs <lucasdeslz@hotmail.com>
2022-06-22 16:50:58 -0300

    Req 3 - mapStateToProps, header, wallet

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Header extends Component {
+  render() {
+    const { email } = this.props;
+    return (
+      <header>
+        <p data-testid="email-field">{email}</p>
+        <p data-testid="total-field">0</p>
+        <p data-testid="header-currency-field">BRL</p>
+      </header>
+    );
+  }
+}
+
+Header.propTypes = {
+  email: PropTypes.string,
+}.isRequired;
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Header);
