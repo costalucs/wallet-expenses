7c516521978c8f658a87d5bc86a3b9cff89b2f98
costalucs <lucasdeslz@hotmail.com>
2022-06-23 16:25:40 -0300

    req 8 - expense table fill

--- src/components/ExpenseTable.jsx
+++ src/components/ExpenseTable.jsx
@@ -1,8 +1,12 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import parseValue from '../services/parseValue';
 
 class ExpenseTable extends Component {
   render() {
+    const { expenses } = this.props;
+    console.log(expenses);
     return (
       <div>
         <th>Descrição</th>
@@ -13,10 +17,33 @@ class ExpenseTable extends Component {
         <th>Câmbio utilizado</th>
         <th>Valor convertido</th>
         <th>Moeda de conversão</th>
-        <th>Editar/Excluir</th>
+        {expenses.map((item, index) => (
+          <tr key={ index }>
+            <td>{item.description}</td>
+            <td>{item.tag}</td>
+            <td>{item.method}</td>
+            <td>{parseFloat(item.value).toFixed(2)}</td>
+            <td>{item.exchangeRates[item.currency].name}</td>
+            <td>{parseFloat(item.exchangeRates[item.currency].ask).toFixed(2)}</td>
+            <td>
+              {parseFloat(parseValue(item.value, item.exchangeRates[item.currency].ask))
+                .toFixed(2)}
+
+            </td>
+            <td>Real</td>
+          </tr>
+        ))}
       </div>
     );
   }
 }
 
-export default connect()(ExpenseTable);
+ExpenseTable.propTypes = {
+  expenses: PropTypes.array,
+}.isRequired;
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(ExpenseTable);
---
+++ src/services/parseValue.js
@@ -0,0 +1,3 @@
+const parseValue = (value, cambioRate) => parseFloat(value) * parseFloat(cambioRate);
+
+export default parseValue;
